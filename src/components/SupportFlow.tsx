f3ffda0618cc14415860b77ad8ed40b1f459d811
Vijendra Rana <vkrana@Vijendras-Mac-mini.local>
2025-06-29 22:36:41 +0530

    tab rename

--- src/components/SupportFlow.tsx
+++ src/components/SupportFlow.tsx
@@ -47,7 +47,7 @@ const SupportFlow: React.FC = () => {
                 : 'text-gray-500 hover:text-gray-700'
             }`}
           >
-            Search & Similar Issues
+            UI Preview
           </motion.button>
           
           <motion.button
